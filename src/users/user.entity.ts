1c54395caa64eb6ed80d74c364c9535cdc7aa3d5
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-06-16 12:00:00 +0200

    Initial commit

---
+++ src/users/user.entity.ts
@@ -0,0 +1,40 @@
+import { Report } from 'src/reports/report.entity';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  OneToMany,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted user with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removed user with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updated user with id', this.id);
+  }
+}
